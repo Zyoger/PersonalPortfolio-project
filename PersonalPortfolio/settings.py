6e4b7e83adf3a60dfe8a9a67ec84c7be97badb14
zyoge <zyoger@gmail.com>
2023-02-26 21:53:40 +0300

    Change main page

--- PersonalPortfolio/settings.py
+++ PersonalPortfolio/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%484oksd89$t$d42mm25=f*_uv-dc%fpm)x1343_m5g5hnvo8ers_g&5e(f'
+SECRET_KEY = 'django-insecure-23525'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["zyoger.ru", "194.34.133.202", "localhost"]
+ALLOWED_HOSTS = ["zyoger.ru", "www.zyoger.ru", "localhost", "zyoger.online", "www.zyoger.online", "127.0.0.1"]
 
 
 # Application definition
@@ -121,6 +122,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATIC_URL = 'static/'
 
