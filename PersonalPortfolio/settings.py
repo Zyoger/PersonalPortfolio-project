ed014122a7c1e6c5fc9013f6655b4d73ac5173a4
zyoge <zyoger@gmail.com>
2023-02-27 14:40:52 +0300

    set static

--- PersonalPortfolio/settings.py
+++ PersonalPortfolio/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-23525'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["zyoger.ru", "www.zyoger.ru", "localhost", "zyoger.online", "www.zyoger.online", "127.0.0.1"]
 
--- portfolio/models.py
+++ portfolio/models.py
@@ -5,7 +5,7 @@ class Project(models.Model):
     title = models.CharField(max_length=100)
     preview = models.CharField(max_length=300)
     description = models.TextField()
-    image = models.ImageField(upload_to="portfolio/images/portfolio/")
+    image = models.ImageField(upload_to="static/image/project/")
     url = models.URLField(blank=True)
 
     def __str__(self):
@@ -15,8 +15,8 @@ class Project(models.Model):
 class Certificate(models.Model):
     title = models.CharField(max_length=50)
     description = models.TextField(max_length=500)
-    image = models.ImageField(upload_to="portfolio/images/certificates/")
-    file = models.FileField(upload_to="portfolio/files/")
+    image = models.ImageField(upload_to="static/images/certificates/")
+    file = models.FileField(upload_to="static/files/")
     url = models.URLField(blank=True)
 
     def __str__(self):
@@ -25,10 +25,10 @@ class Certificate(models.Model):
 
 class InformationAboutMe(models.Model):
     my_nick = models.CharField(max_length=50)
-    my_photo = models.ImageField(upload_to="portfolio/info/")
+    my_photo = models.ImageField(upload_to="static/images/")
     description_main = models.TextField()
     description_second = models.TextField(blank=True)
-    my_resume = models.FileField(upload_to="portfolio/info/")
+    my_resume = models.FileField(upload_to="static/files/")
     my_phone_number = models.CharField(max_length=20)
     link_telegram = models.URLField(blank=True)
     link_instagram = models.URLField(blank=True)
